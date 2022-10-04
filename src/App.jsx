954795feccc998e3436e4a0bbca31f35ffd5be98
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-10-04 13:08:14 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <>
+
+      <Header />
+      <Nav />
+      
+    <div> WHAT THE FUCK </div>
+
+
+    </>
+  )
+}
+
+export default App
