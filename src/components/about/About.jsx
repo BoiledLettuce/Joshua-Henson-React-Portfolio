29720926592a7a80a8974686f6bb32b07240ee37
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-10-04 17:03:22 -0700

    navbar, me image, about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.png'
+import {TbAward} from 'react-icons/tb'
+import {RiTeamFill} from 'react-icons/ri'
+import {GiCubes} from 'react-icons/gi'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+
+    <h5>Get to Know</h5>
+    <h2>About Me Bro</h2>
+
+    <div className="container about__container"></div>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        
+      <div className="about__content">
+        <div className="about__cards">
+
+          <article className="about__card">
+            <TbAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>300+ Years Working</small>
+          </article>
+
+          <article className="about__card">
+            <RiTeamFill className='about__icon'/>
+            <h5>Clients</h5>
+            <small>300,000+ Worldwide</small>
+          </article>
+
+          <article className="about__card">
+            <GiCubes className='about__icon'/>
+            <h5>Projects</h5>
+            <small>9000+ Completed Projects</small>
+          </article>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
